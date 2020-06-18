10e788cd22a05223a1f9f08f5c97c95157db362a
Jayson Silva <jaysonwcs@gmail.com>
2020-06-18 19:09:18 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
