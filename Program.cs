a006a80c5e31941c47723bfe54b619b57c3fff92
Jayson Silva <jaysonwcs@gmail.com>
2020-06-19 11:38:27 -0300

    Exercise finished

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Exercise15_2
 {
@@ -6,7 +8,40 @@ namespace Exercise15_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+
+            try
+            {
+                using StreamReader reader = File.OpenText(path);
+                while (!reader.EndOfStream)
+                {
+                    string[] values = reader.ReadLine().Split(',');
+
+                    if (votes.ContainsKey(values[0]))
+                    {
+                        votes[values[0]] += int.Parse(values[1]);
+                    }
+                    else
+                    {
+                        votes[values[0]] = int.Parse(values[1]);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred:");
+                Console.WriteLine(e.Message);
+            }
+
+
+            foreach (var item in votes)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
         }
     }
 }
